223c1c4f37ae85958ac0d7b88027336e0319dc78
Irina-dav <iren@IREN-DESKTOP>
2018-10-11 23:09:20 +0800

    refactoring: new class ZendObject, new defs

--- zend.py
+++ zend.py
@@ -13,8 +13,6 @@ from telegram.ext import Updater
 
 import config
 
-tzu = tzutc()
-
 url_api = f'https://{config.zend_subdomain}.zendesk.com/api/v2/'
 url_posts = url_api + 'community/posts.json?sort_by=updated_at&include=topics'
 url_post_comments = url_api + 'community/posts/{post_id}/comments.json'
@@ -35,27 +33,40 @@ logging.basicConfig(
     ])
 
 
-class Article():
-    def __init__(self, json_data, sections_dict):
+class ZendObject():
+    def __init__(self, json_data):
         self.id = json_data['id']
         self.title = json_data['title']
         self.updated_at = parse(json_data['updated_at'])
         self.created_at = parse(json_data['created_at'])
         self.html_url = json_data['html_url']
-        self.section = sections_dict[json_data['section_id']]
+
+    def __repr__(self):
+        return f'{self.title} - {self.html_url}'
 
 
-class Post():
-    def __init__(self, json_data, topics_dict):
-        self.id = json_data['id']
-        self.title = json_data['title']
-        self.updated_at = parse(json_data['updated_at'])
-        self.created_at = parse(json_data['created_at'])
-        self.html_url = json_data['html_url']
-        self.topic = topics_dict[json_data['topic_id']]
+class Article(ZendObject):
+    def __init__(self, json_data, section):
+        super().__init__(json_data)
+        self.section = section
+
+    def repr_html(self):
+        return f'[{self.section}]\n{self.title}\
+                <a href="{self.html_url}">\nЧитать статью</a>'
+
+
+class Post(ZendObject):
+    def __init__(self, json_data, topic):
+        super().__init__(json_data)
+        self.topic = topic
+
+    def repr_html(self):
+        return f'[{self.topic}]\n{self.title}\
+                <a href="{self.html_url}">\nЧитать пост</a>'
 
 
 def get_start_date():
+    tzu = tzutc()
     with shelve.open(config.shelve_name) as db:
         start_date = db.get(
             'start_date', datetime.now().astimezone(tzu) - timedelta(days=7))
@@ -93,21 +104,28 @@ def send_to_telegram(*messages):
         raise ex
 
 
-def get_z_objects(api_url, cls, start_date):
+def create_articles_from_json(json_data):
+    sections_dict = {s['id']: s['name'] for s in json_data['sections']}
+    return [Article(a, sections_dict[a['section_id']])
+            for a in json_data['articles']]
+
+
+def create_posts_from_json(json_data, start_date):
+    topics_dict = {t['id']: t['name'] for t in json_data['topics']}
+    return [Post(p, topics_dict[p['topic_id']]) for p in json_data['posts']
+            if parse(p['updated_at']) > start_date]
+
+
+def get_zend_objects(api_url, cls, start_date):
     cls_name = cls.__name__
-    data_objects = fetch_data(api_url)
-    if data_objects is None or data_objects['count'] == 0:
+    json_data = fetch_data(api_url)
+    if json_data is None or json_data['count'] == 0:
         objects = []
     else:
         if cls is Article:
-            sections_dict = {s['id']: s['name']
-                             for s in data_objects['sections']}
-            objects = [Article(a, sections_dict)
-                       for a in data_objects['articles']]
+            objects = create_articles_from_json(json_data)
         elif cls is Post:
-            topics_dict = {t['id']: t['name'] for t in data_objects['topics']}
-            objects = [Post(p, topics_dict) for p in data_objects['posts']
-                       if parse(p['updated_at']) > start_date]
+            objects = create_posts_from_json(json_data, start_date)
 
     logging.info(f'{cls_name}s, fetched count: {len(objects)}')
 
@@ -135,46 +153,39 @@ def get_z_objects(api_url, cls, start_date):
     return (objects_new, objects_with_new_comm)
 
 
+def create_html_block(title, elements):
+    return f'[<b>{title}:</b>\n\n' + '\n\n'.join(
+        [elem.repr_html() for elem in elements])
+
 if __name__ == '__main__':
     html_articles = html_articles_new_comm = \
         html_posts_new = html_posts_new_comm = None
 
     start_date = get_start_date()
-    start_date_unix = int(time.mktime(start_date.timetuple()))
-    logging.info(f'Start date: {start_date}')
+    timestamp = int(start_date.timestamp())
+    logging.info(f'Start date: {start_date}, timestamp: {timestamp}')
 
-    articles_new, articles_with_new_comm = get_z_objects(
-        url_articles.format(start_time=start_date_unix), Article, start_date)
+    articles_new, articles_with_new_comm = get_zend_objects(
+        url_articles.format(start_time=timestamp), Article, start_date)
 
     if articles_new:
-        html_articles = \
-            '<b>Обновления в статьях:</b>\n\n' + '\n\n'.join(
-                [f'[{a.section}]\n{a.title}\
-                 <a href="{a.html_url}">\nЧитать статью</a>'
-                 for a in articles_new])
+        html_articles = create_html_block('Обновления в статьях', articles_new)
 
     if articles_with_new_comm:
-        html_articles_new_comm =\
-            '<b>Cтатьи с новыми комментариями:</b>\n\n' + '\n\n'.join(
-                [f'[{a.section}]\n{a.title}\
-                 <a href="{a.html_url}">\nЧитать статью</a>'
-                 for a in articles_with_new_comm])
+        html_articles_new_comm = create_html_block(
+            'Cтатьи с новыми комментариями',
+            articles_with_new_comm)
 
-    posts_new, posts_with_new_comm = get_z_objects(url_posts, Post, start_date)
+    posts_new, posts_with_new_comm = get_zend_objects(
+        url_posts, Post, start_date)
 
     if posts_new:
-        html_posts_new = \
-            '<b>Новые посты:</b>\n\n' + '\n\n'.join(
-                [f'[{p.topic}]\n{p.title}\
-                 <a href="{p.html_url}">\nЧитать пост</a>'
-                 for p in posts_new])
+        html_posts_new = create_html_block('Новые посты', posts_new)
 
     if posts_with_new_comm:
-        html_posts_new_comm = \
-            '<b>Посты с новыми комментариями:</b>\n\n' + '\n\n'.join(
-                [f'[{p.topic}]\n{p.title}\
-                 <a href="{p.html_url}">\nЧитать пост</a>'
-                 for p in posts_with_new_comm])
+        html_posts_new_comm = create_html_block(
+            'Посты с новыми комментариями',
+            posts_with_new_comm)
 
     send_to_telegram(html_articles, html_articles_new_comm,
                      html_posts_new, html_posts_new_comm)
