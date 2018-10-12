08cda6940ae0a53c9bc255393c621dba8513d5aa
Irina-dav <iren@IREN-DESKTOP>
2018-10-12 11:37:10 +0800

    refactoring

--- zend.py
+++ zend.py
@@ -15,17 +15,17 @@ import config
 
 url_api = f'https://{config.zend_subdomain}.zendesk.com/api/v2/'
 url_posts = url_api + 'community/posts.json?sort_by=updated_at&include=topics'
-url_post_comments = url_api + 'community/posts/{post_id}/comments.json'
+url_post_comments = url_api + 'community/posts/{object_id}/comments.json'
 url_post = url_api + 'community/posts/{post_id}.json'
 url_articles = url_api + (
     'help_center/incremental/articles.json?start_time={start_time}'
     '&include=sections')
 url_article_comments = url_api + \
-    'help_center/ru/articles/{article_id}/comments.json'
+    'help_center/ru/articles/{object_id}/comments.json'
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
-    level=logging.INFO,
+    level=logging.DEBUG,
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
             logging.FileHandler('zend.log'),
@@ -79,7 +79,7 @@ def upd_start_date(new_date):
         db['start_date'] = new_date
 
 
-def fetch_data(url):
+def fetch_url(url):
     response = requests.get(url, auth=(config.zend_user, config.zend_pwd))
     if response.status_code != 200:
         logging.error(
@@ -117,41 +117,55 @@ def create_posts_from_json(json_data, start_date):
             if parse(p['updated_at']) > start_date]
 
 
-def get_zend_objects(api_url, cls, start_date):
-    cls_name = cls.__name__
-    json_data = fetch_data(api_url)
-    if json_data is None or json_data['count'] == 0:
-        objects = []
+def create_objects_from_json(json_data, start_date):
+    if json_data['count'] == 0:
+        return []
+    if 'articles' in json_data:
+        return create_articles_from_json(json_data)
+    elif 'posts' in json_data:
+        return create_posts_from_json(json_data, start_date)
     else:
-        if cls is Article:
-            objects = create_articles_from_json(json_data)
-        elif cls is Post:
-            objects = create_posts_from_json(json_data, start_date)
+        raise Exception('Wrong format of json data')
 
-    logging.info(f'{cls_name}s, fetched count: {len(objects)}')
+
+def get_zend_new_upd_objects(api_url, start_date):
+    logging.info(f'Fetch url {api_url}')
+    json_data = fetch_url(api_url)
+
+    objects = create_objects_from_json(json_data, start_date)
+    logging.info(f'Fetched objects count: {len(objects)}')
 
     objects_new = [o for o in objects if o.created_at >= start_date]
+    logging.info(f'New objects count: {len(objects_new)}')
+
     objects_upd = [o for o in objects if o.created_at < start_date]
+    logging.info(f'Updated objects count: {len(objects_upd)}')
 
-    logging.info(f'{cls_name}s, new articles count: {len(objects_new)}')
-    logging.info(f'{cls_name}s, updated articles count: {len(objects_upd)}')
+    return (objects_new, objects_upd)
 
+
+def get_zend_new_comments(objects_upd, start_date):    
+    if not objects_upd:
+        return []
+
+    urls = {'Article': url_article_comments, 'Post': url_post_comments}
+    try:
+        cls_name = objects_upd[0].__class__.__name__
+        logging.debug(f'cls_name={cls_name}')
+        url = urls[cls_name]
+    except KeyError:
+        raise KeyError('Error getting comment url')
     objects_with_new_comm = []
     for o in objects_upd:
-        if cls is Article:
-            data_comments = fetch_data(
-                url_article_comments.format(article_id=o.id))
-        elif cls is Post:
-            data_comments = fetch_data(url_post_comments.format(post_id=o.id))
-
+        json_comments = fetch_url(url.format(object_id=o.id))
         if any(parse(comm['updated_at']) > start_date
-                for comm in data_comments['comments']):
+                for comm in json_comments['comments']):
                     objects_with_new_comm.append(o)
 
     logging.info(
         f'{cls_name}s, with new comments count: {len(objects_with_new_comm)}')
 
-    return (objects_new, objects_with_new_comm)
+    return objects_with_new_comm
 
 
 def create_html_block(title, elements):
@@ -166,27 +180,26 @@ if __name__ == '__main__':
     timestamp = int(start_date.timestamp())
     logging.info(f'Start date: {start_date}, timestamp: {timestamp}')
 
-    articles_new, articles_with_new_comm = get_zend_objects(
-        url_articles.format(start_time=timestamp), Article, start_date)
+    articles_new, articles_upd = get_zend_new_upd_objects(
+        url_articles.format(start_time=timestamp), start_date)
+    articles_with_new_comm = get_zend_new_comments(articles_upd, start_date)
 
     if articles_new:
         html_articles = create_html_block('Обновления в статьях', articles_new)
 
     if articles_with_new_comm:
         html_articles_new_comm = create_html_block(
-            'Cтатьи с новыми комментариями',
-            articles_with_new_comm)
+            'Cтатьи с новыми комментариями', articles_with_new_comm)
 
-    posts_new, posts_with_new_comm = get_zend_objects(
-        url_posts, Post, start_date)
+    posts_new, posts_upd = get_zend_new_upd_objects(url_posts, start_date)
+    posts_with_new_comm = get_zend_new_comments(posts_upd, start_date)
 
     if posts_new:
         html_posts_new = create_html_block('Новые посты', posts_new)
 
     if posts_with_new_comm:
         html_posts_new_comm = create_html_block(
-            'Посты с новыми комментариями',
-            posts_with_new_comm)
+            'Посты с новыми комментариями', posts_with_new_comm)
 
     send_to_telegram(html_articles, html_articles_new_comm,
                      html_posts_new, html_posts_new_comm)
